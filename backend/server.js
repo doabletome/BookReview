a6227ffa14c98e5229dc9ad8509adb87ef05acbe
doabletome <patlehimanshu98@gmail.com>
2025-06-20 19:17:50 +0530

    server

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:5173", // your React app URL
+    origin: process.env.FRONTEND_URL, // your React app URL
     credentials: true, // allow cookies, auth headers
   })
 );
